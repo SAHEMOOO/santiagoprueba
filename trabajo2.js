5fb26425d5626c8346eff1bd5c2e9480e7b02e1c
Santiago <shenaom0303@gmail.com>
2023-07-31 08:57:47 -0500

    caso1

---
+++ trabajo2.js
@@ -0,0 +1,5 @@
+let num1 = parseInt(prompt("ingresa el primer numero"));
+let num2 = parseInt(prompt("ingresa el segundo numero"));
+let promedio =(num1 + num2) /2;
+let promRound= Math.round(promedio);
+document.write(`El promedio es: ${promRound}`);
